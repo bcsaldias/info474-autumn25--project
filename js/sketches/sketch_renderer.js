9e268501f713f0d99f8b7db29f08181c7d617a1d
Yongxi Chen <yc122@uw.edu>
2025-11-19 14:43:36 -0800

    vis_1 complete, update css(background color, font/font color)

--- js/sketches/sketch_renderer.js
+++ js/sketches/sketch_renderer.js
@@ -26,6 +26,11 @@
                 return;
             }
 
+            if (ai >= 2 && ai <= 4) {
+                window.VizTravel.draw(p, manager, ai - 2, progress);
+                return;
+            }
+
             if (ai >= 4 && ai < 7) {
                 window.VizScatter.draw(p, manager, ai, progress);
                 return;
