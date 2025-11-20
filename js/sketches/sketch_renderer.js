ae53468f64cb33b89e026014366c67725ceaf406
Cole Meier <colemeier@Coles-MacBook-Pro-8.local>
2025-11-20 12:30:17 -0800

    inital draft of scale visualization

--- js/sketches/sketch_renderer.js
+++ js/sketches/sketch_renderer.js
@@ -26,7 +26,12 @@
                 return;
             }
 
-            if (ai >= 4 && ai < 7) {
+            if (ai >= 2 && ai < 5) {
+                window.VizJustice.draw(p, manager, ai, progress);
+                return;
+            }
+
+            if (ai >= 5 && ai < 7) {
                 window.VizScatter.draw(p, manager, ai, progress);
                 return;
             }
