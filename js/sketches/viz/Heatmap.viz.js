8d63510a0236228e0edb7c989e00010955e4d481
sohasultana <107889414+sohasultana@users.noreply.github.com>
2025-11-19 16:07:57 -0800

    added countries and tariff rates
    Add comments explaining the heatmap visualization.

---
+++ js/sketches/viz/Heatmap.viz.js
@@ -0,0 +1,18 @@
+// heatmap using a treemap style to categorize tariff percentage by countries
+// through different tile shapes
+(function(){
+    window.VizTreemap = {};
+
+    VizTreemap.defaultData = {
+        Brazil: 7,
+        Canada: 1,
+        China: 2,
+        Germany: 1,
+        UK: 1,
+        India: 6,
+        Japan: 1,
+        Korea: 6,
+        Mexico: 2,
+        Nigeria: 9,
+        SA: 5
+    };
