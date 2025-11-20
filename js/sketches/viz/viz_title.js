bd3f8e5d82106116f535d9053f3335f4e67d6935
Yongxi Chen <yc122@uw.edu>
2025-11-20 11:27:10 -0800

    Update viz_title color and font

--- js/sketches/viz/viz_title.js
+++ js/sketches/viz/viz_title.js
@@ -7,12 +7,14 @@
             var cy = (manager.offsetY || 0) + (manager.height || 520) / 3;
             p.push();
             p.noStroke();
-            p.fill(255);
+            p.fill(4, 17, 34);
             var w = 420;
             var h = 120;
             p.rect(cx - w / 2, cy - h / 2, w, h, 6);
 
-            p.fill(0);
+            p.fill('#E6F99D');
+            p.textFont('Unbounded');
+
             p.textAlign(p.CENTER, p.CENTER);
             p.textSize(48);
             p.text(ai === 0 ? 'INFO 474' : 'Final Project', cx, cy);
