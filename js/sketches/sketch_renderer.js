f63ad697e9a01914347b2739f1fcefbae503a8d9
gardnas <sdgard863@gmail.com>
2025-11-25 23:05:57 -0800

    update render for number line vis

--- js/sketches/sketch_renderer.js
+++ js/sketches/sketch_renderer.js
@@ -40,13 +40,18 @@
                 window.VizJustice.draw(p, manager, ai, progress);
                 return;
             }
-
-            if (ai >= 5 && ai < 7) {
-                window.VizScatter.draw(p, manager, ai, progress);
+            
+             if (ai === 6) {
+                window.VizStateNumberLine.draw(p, manager, ai, progress);
                 return;
-            }
+            } 
 
-            if (ai === 7) {
+            // if (ai >= 6 && ai < 7) {
+            //     window.VizScatter.draw(p, manager, ai, progress);
+            //     return;
+            // }
+
+            if (ai >= 7) {
                 window.VizExploreMoreMap.draw(p, manager, ai, progress);
                 return;
             }
